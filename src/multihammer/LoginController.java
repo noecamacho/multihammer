dcf6c4f45b7f5afd2457d1ca104dd73c18c4d8ca
noecl <noecl1@hotmail.com>
2019-02-20 20:21:07 -0700

    Login view created and its controller

---
+++ src/multihammer/LoginController.java
@@ -0,0 +1,19 @@
+package multihammer;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+// FXML Clase del Controlador "Login"
+public class LoginController implements Initializable {
+   
+    // Inicializa la clase del controlador
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }
+    public void Login(){
+        System.out.println("Ingresando");
+    }
+    
+}
