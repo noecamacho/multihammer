9ede0798396d4116bc72896c9b8926153f0e0762
Kevin Lizarraga <2016030221@upsin.edu.mx>
2019-03-10 21:14:03 -0700

    Add/Modify providers/Clients

---
+++ src/Controller/ClientesController.java
@@ -0,0 +1,149 @@
+package Controller;
+
+import Model.Cliente;
+import Model.ClientesModel;
+import Model.Dialogs;
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import com.jfoenix.controls.JFXTreeTableColumn;
+import com.jfoenix.controls.JFXTreeTableView;
+import com.jfoenix.controls.RecursiveTreeItem;
+import com.jfoenix.controls.datamodels.treetable.RecursiveTreeObject;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.beans.value.ChangeListener;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TreeItem;
+import javafx.scene.control.TreeTableColumn;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+public class ClientesController implements Initializable {
+
+    private final ClientesModel modelo = new ClientesModel();
+    final private Dialogs dialogs = new Dialogs();
+    private Cliente selectedCliente = new Cliente();
+    
+    @FXML
+    private JFXButton btnAgregar;
+    @FXML
+    private JFXButton btnModificar;
+    @FXML
+    private JFXButton btnEliminar;
+    @FXML
+    private JFXTreeTableView<Cliente> table;
+    @FXML
+    private JFXTextField searchField; 
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        createTable();
+        createTableView();
+    }    
+    
+    private Cliente getLeadSelect() {
+        TreeItem<Cliente> selectedItem = (TreeItem<Cliente>) table.getSelectionModel().getSelectedItem();
+        return selectedItem == null ? null : selectedItem.getValue();
+    }
+    
+    private ChangeListener<String> setupSearchField(final JFXTreeTableView<Cliente> tableView) {
+        return (o, oldVal, newVal) ->
+            tableView.setPredicate(personProp -> {
+                final Cliente prod = personProp.getValue();
+                return prod.nombre.get().contains(newVal)
+                       || prod.apellido.get().contains(newVal);
+            });
+    }
+    
+    public void createTable() {
+        
+        table.getSelectionModel()
+                .selectedItemProperty()
+                .addListener((observable, oldValue, newValue) -> this.selectedCliente = getLeadSelect());
+        
+        searchField.textProperty().addListener(setupSearchField(table));
+
+        JFXTreeTableColumn<Cliente, String> id = new JFXTreeTableColumn("ID");
+        id.setPrefWidth(100);
+        id.setCellValueFactory((TreeTableColumn.CellDataFeatures<Cliente, String> param) -> param.getValue().getValue().id_cliente);
+
+        JFXTreeTableColumn<Cliente, String> nombre = new JFXTreeTableColumn("Nombre");
+        nombre.setPrefWidth(100);
+        nombre.setCellValueFactory((TreeTableColumn.CellDataFeatures<Cliente, String> param) -> param.getValue().getValue().nombre);
+
+        JFXTreeTableColumn<Cliente, String> apellido = new JFXTreeTableColumn("Apellido");
+        apellido.setPrefWidth(100);
+        apellido.setCellValueFactory((TreeTableColumn.CellDataFeatures<Cliente, String> param) -> param.getValue().getValue().apellido);
+
+        JFXTreeTableColumn<Cliente, String> domicilio = new JFXTreeTableColumn("Domicilio");
+        domicilio.setPrefWidth(100);
+        domicilio.setCellValueFactory((TreeTableColumn.CellDataFeatures<Cliente, String> param) -> param.getValue().getValue().domicilio);
+
+        JFXTreeTableColumn<Cliente, String> telefono = new JFXTreeTableColumn("Teléfono");
+        telefono.setPrefWidth(100);
+        telefono.setCellValueFactory((TreeTableColumn.CellDataFeatures<Cliente, String> param) -> param.getValue().getValue().telefono);
+
+        JFXTreeTableColumn<Cliente, String> rfc = new JFXTreeTableColumn("RFC");
+        rfc.setPrefWidth(100);
+        rfc.setCellValueFactory((TreeTableColumn.CellDataFeatures<Cliente, String> param) -> param.getValue().getValue().rfc);
+
+
+        table.getColumns().setAll(id, nombre, apellido, domicilio, telefono, rfc);
+    }
+    
+    public void createTableView() {
+        TreeItem<Cliente> root = new RecursiveTreeItem<>(tableInformation(), RecursiveTreeObject::getChildren);
+        table.setRoot(root);
+        table.setShowRoot(false);
+    }
+
+    public ObservableList<Cliente> tableInformation() {
+        ObservableList<Cliente> proveedor = FXCollections.observableArrayList();
+        modelo.getClientes().forEach((x) -> {
+            proveedor.add(new Cliente(x.id_cliente, x.domicilio, x.rfc, x.nombre,x.apellido, x.telefono));
+        });
+        return proveedor;
+    }
+    
+
+    @FXML
+    private void btnAgregarAction(ActionEvent event) throws IOException {
+        Stage stage = new Stage();
+        FXMLLoader modal = new FXMLLoader(getClass().getResource("/View/ClientesModal.fxml"));
+        Parent root = modal.load();
+        stage.setScene(new Scene(root));
+        stage.setTitle("Agregar Clientes");
+        stage.initModality(Modality.WINDOW_MODAL);
+        stage.initOwner(((Node)event.getSource()).getScene().getWindow() );
+        stage.showAndWait();
+        createTableView();
+    }
+
+    @FXML
+    private void btnModificarAction(ActionEvent event) throws IOException {
+        Stage stage = new Stage();
+        FXMLLoader modal = new FXMLLoader(getClass().getResource("/View/ClientesModal.fxml"));
+        Parent root = modal.load();
+        ClientesModalController pmc = modal.getController();
+        pmc.setValuesToModify(selectedCliente.id_cliente.get(), selectedCliente.domicilio.get(), selectedCliente.rfc.get(), selectedCliente.nombre.get(),selectedCliente.apellido.get(), selectedCliente.telefono.get());
+        stage.setScene(new Scene(root));
+        stage.setTitle("Agregar Producto");
+        stage.initModality(Modality.WINDOW_MODAL);
+        stage.initOwner(((Node)event.getSource()).getScene().getWindow() );
+        stage.showAndWait();
+        createTableView();
+    }
+
+}
---
+++ src/Controller/ClientesModalController.java
@@ -0,0 +1,101 @@
+package Controller;
+
+import Model.ClientesModel;
+import Model.Dialogs;
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class ClientesModalController implements Initializable {
+
+    final private ClientesModel modelo = new ClientesModel();
+    final private Dialogs dialogs = new Dialogs();
+    
+    @FXML
+    private StackPane root;
+    @FXML
+    private Label labelHeader;
+    @FXML
+    private JFXTextField txtNombre;
+    @FXML
+    private JFXTextField txtApellido;
+    @FXML
+    private JFXTextField txtDomicilio;
+    @FXML
+    private JFXTextField txtTelefono;
+    @FXML
+    private JFXTextField txtRFC;
+    @FXML
+    private JFXButton btnAgregar;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        btnAgregar.setOnAction((event) -> {
+            agregar();
+        });
+        txtTelefono.textProperty().addListener((observable, oldValue, newValue) -> {
+            if (!newValue.matches("\\d*")) {
+                txtTelefono.setText(newValue.replaceAll("[^\\d]", ""));
+            }
+        });
+        txtRFC.textProperty().addListener((observable, oldValue, newValue) -> {
+            if (!newValue.matches("[A-Z\\d]{12,13}")) {
+                txtRFC.setText(newValue.replaceAll("[^A-Z\\d]{12,13}", ""));
+            }
+        });
+        txtNombre.textProperty().addListener((observable, oldValue, newValue) -> {
+            if (!newValue.matches("a-zA-ZñÑáéíóúÁÉÍÓÚ\\s*")) {
+                txtNombre.setText(newValue.replaceAll("[^a-zA-ZñÑáéíóúÁÉÍÓÚ\\s]*", ""));
+            }
+        });
+        txtApellido.textProperty().addListener((observable, oldValue, newValue) -> {
+            if (!newValue.matches("a-zA-ZñÑáéíóúÁÉÍÓÚ\\s*")) {
+                txtApellido.setText(newValue.replaceAll("[^a-zA-ZñÑáéíóúÁÉÍÓÚ\\s]*", ""));
+            }
+        });
+    }
+
+    public void agregar() {
+        if (!txtNombre.getText().equals("") && !txtApellido.getText().equals("") &&!txtDomicilio.getText().equals("") && !txtTelefono.getText().equals("") && !txtRFC.getText().equals("")) {
+            if(txtRFC.getText().matches("[A-ZÑ&]{3,4}\\d{6}[A-V1-9][A-Z1-9][0-9A]")) {
+                modelo.agregarCliente(txtNombre.getText(), txtApellido.getText(), txtDomicilio.getText(), txtTelefono.getText(), txtRFC.getText());
+                Stage stage = (Stage) btnAgregar.getScene().getWindow();
+                stage.close();
+            } else {
+                dialogs.displayMessage((Stage) btnAgregar.getScene().getWindow(), "Advertencia", "Introduce un RFC válido", "OK");
+            }
+        } else {
+            dialogs.displayMessage((Stage) btnAgregar.getScene().getWindow(), "Advertencia", "Para agregar un cliente es necesario que todos los campos se encuentren llenos", "OK");
+        }
+    }
+
+    public void modificar(String id_cliente) {
+        if (!txtNombre.getText().equals("") && !txtApellido.getText().equals("") &&!txtDomicilio.getText().equals("") && !txtTelefono.getText().equals("") && !txtRFC.getText().equals("")) {
+            modelo.modificarCliente(txtNombre.getText(), txtApellido.getText(), txtDomicilio.getText(), txtTelefono.getText(), txtRFC.getText(), id_cliente);
+            Stage stage = (Stage) btnAgregar.getScene().getWindow();
+            stage.close();
+        } else {
+            dialogs.displayMessage((Stage) btnAgregar.getScene().getWindow(), "Advertencia", "Para modificar un cliente es necesario que todos los campos se encuentren llenos", "OK");
+        }
+    }
+    
+    public void setValuesToModify(String id_cliente, String domicilio, String rfc, String nombre, String apellido, String telefono) {
+        txtDomicilio.setText(domicilio);
+        txtRFC.setText(rfc);
+        txtNombre.setText(nombre);
+        txtApellido.setText(apellido);
+        txtTelefono.setText(telefono);
+        labelHeader.setText("Modificar Cliente");
+        btnAgregar.setText("Modificar");
+        btnAgregar.setOnAction((event) ->{
+            modificar(id_cliente);
+        });
+    }   
+    
+}
